28d6549f0ef05e818cc9eacdd1a01d85a8f5fa3c
lmitchell524 <lmitchell524@gmail.com>
2018-03-01 15:42:37 -0800

    made edits to proxy server

--- resources/proxy.php
+++ resources/proxy.php
@@ -1,7 +1,13 @@
 <?php
-    if (strpos($_POST['url'], 'http://superheroapi.com/api.php/10159579732380612/') !== false) {
+    $url = filter_var($_POST['url'], FILTER_VALIDATE_URL);
+
+    if(empty($url)){
+        exit(400);
+    };
+
+    if (strpos($url, 'http://superheroapi.com/api.php') === 0) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, filter_var($_POST['url'], FILTER_VALIDATE_URL));
+        curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, 'Access-Control-Allow-Origin: *');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $return = curl_exec($ch);
